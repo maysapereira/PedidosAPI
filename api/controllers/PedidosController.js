04a94c908a7962b07b0887fd5ceaad406d29a8f0
Maysa Pereira <maysalvespereira@gmail.com>
2022-03-19 19:08:14 -0300

    criacao de controller e ponto de entrada das rotas

---
+++ api/controllers/PedidosController.js
@@ -0,0 +1,16 @@
+const database = require('../models')
+
+class PedidosController {
+    static async pegaPedidos(req, res){
+        try{
+            const todosOsPedidos = await database.Pedidos.findAll()
+            return res.status(200).json(todosOsPedidos)
+        } catch (error){
+            return res.status(500).json(error.message)
+        }
+
+    }
+
+}
+
+module.exports = PessoaController
